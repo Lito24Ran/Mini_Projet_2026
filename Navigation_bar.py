eadaf3528ec20d3bf6ebc00520055d0856bcd157
Lito24Ran <randrianaivosonazaria@gmail.com>
2026-02-25 22:38:21 +0300

    changement dans la barre de navigation/design/front25-02-26

--- Navigation_bar.py
+++ Navigation_bar.py
@@ -2,7 +2,8 @@ from kivy .lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout  import GridLayout
 Builder.load_file('navigation_bar.kv')
 
-class NavigationBar(Widget) :
+class NavigationBar(FloatLayout) :
     pass
---
+++ bar_de_recheche.py
@@ -0,0 +1,6 @@
+from kivy .lang import Builder
+
+Builder.load_file('navigation_bar.kv')
+
+class bar_de_recherche():
+    pass
