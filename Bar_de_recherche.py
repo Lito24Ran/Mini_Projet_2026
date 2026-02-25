d74767b9c32e0ee042d4006fd80a0afd30baa319
Lito24Ran <randrianaivosonazaria@gmail.com>
2026-02-25 23:28:49 +0300

    barre de recherche mis en place

---
+++ Bar_de_recherche.py
@@ -0,0 +1,7 @@
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
+
+Builder.load_file('bar_de_recherche.kv')
+
+class Bar_de_recherche(FloatLayout):
+    pass
--- bar_de_recheche.py
+++
@@ -1,6 +0,0 @@
-from kivy .lang import Builder
-
-Builder.load_file('navigation_bar.kv')
-
-class bar_de_recherche():
-    pass
